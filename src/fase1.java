36b99df66e98ee6bb182892f12678d40ee00fc40
fsarrias <fsarrias@kolab.es>
2020-04-23 10:22:42 +0200

    subo la fase1

---
+++ src/fase1.java
@@ -0,0 +1,14 @@
+public class fase1 {
+    public static void main(String[] args) {
+        int bill_5euros = 5;
+        int bill_10euros = 10;
+        int bill_20euros = 20;
+        int bill_50euros = 50;
+        int bill_100euros = 100;
+        int bill_200euros = 200;
+        int bill_500euros = 500;
+        int total_bill = 0;
+        String[] menu = new String[5];
+        int[] price = new int[5];
+    }
+}
